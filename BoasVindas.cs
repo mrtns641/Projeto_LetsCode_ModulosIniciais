f8ba6a571b1fc82ee065ef90ed6e028bb5396948
Gabriella <gabmcavalheiro641@gmail.com>
2021-09-17 15:25:08 -0300

    Introdução à Classes e Métodos
    Estudo sobre classes e métodos

---
+++ BoasVindas.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ModulosIniciais
+{
+    static class BoasVindas {
+        static public void MensagemBoasVindas() {
+            const string MensagemBoasVindas = "Hello World";
+            Console.WriteLine(MensagemBoasVindas);
+        }
+    }
+}
